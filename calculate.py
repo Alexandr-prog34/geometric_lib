f46c1f9b54e4fc109e7037e441d9bf4da6ab20f2
Alexandr <slender.0034@gmail.com>
2024-11-13 16:19:14 +0300

    tr14

--- calculate.py
+++ calculate.py
@@ -14,7 +14,8 @@ FUNCS = ['perimeter', 'area']
 def calc(fig, func, size):
     if fig not in FIGS:
         raise ValueError(
-            f"Фигура '{fig}' недоступна. Доступные фигуры: {list(FIGS.keys())}"
+            f"Фигура '{fig}' недоступна. Доступные фигуры: "
+            f"{list(FIGS.keys())}"
         )
     if func not in FUNCS:
         raise ValueError(
@@ -27,8 +28,8 @@ def calc(fig, func, size):
     expected_params = FIGS[fig]['params']
     if len(size) != expected_params:
         raise ValueError(
-            f"Для фигуры '{fig}' требуется {expected_params} параметр(ов), но было "
-            f"передано {len(size)}"
+            f"Для фигуры '{fig}' требуется {expected_params} параметр(ов), но "
+            f"было передано {len(size)}"
         )
 
     result = func_to_call(*size)
